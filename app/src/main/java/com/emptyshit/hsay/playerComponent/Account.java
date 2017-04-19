5ea7680800be2bfe99e98e40afb4633b8df264dc
Alex <alex.duong@haw-hamburg.de>
2017-04-19 20:33:16 +0200

    EnterWordGame

--- app/src/main/java/com/emptyshit/hsay/playerComponent/Account.java
+++
@@ -1,8 +0,0 @@
-package com.emptyshit.hsay.playerComponent;
-
-/**
- * Created by Alex on 17.04.2017.
- */
-
-public class Account extends Player {
-}
---
+++ app/src/main/java/com/emptyshit/hsay/playerComponent/PlayerActivity.java
@@ -0,0 +1,8 @@
+package com.emptyshit.hsay.playerComponent;
+
+/**
+ * Created by Alex on 18.04.2017.
+ */
+
+public class PlayerActivity {
+}
