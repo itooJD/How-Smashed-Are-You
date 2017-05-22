35c0d215e80b04949984079e24dbd3eaf51ec6a0
Tu <tu.nguyenanh@haw-hamburg.de>
2017-05-22 21:36:49 +0200

    irgendwie wurde das nicht so richtig gespeichert

--- app/src/main/java/com/emptyshit/hsay/timeMeasureComponent/TimeDataRepository.java
+++ app/src/main/java/com/emptyshit/hsay/timeMeasureComponent/TimeDataRepository.java
@@ -2,6 +2,8 @@ package com.emptyshit.hsay.timeMeasureComponent;
 
 import com.emptyshit.hsay.dataTypes.TimeType;
 import com.emptyshit.hsay.playerComponent.DaoSession;
+import com.emptyshit.hsay.playerComponent.Player;
+import com.emptyshit.hsay.playerComponent.PlayerDao;
 
 import org.greenrobot.greendao.query.QueryBuilder;
 
@@ -16,29 +18,34 @@ public class TimeDataRepository {
         this.timeDataDao = daoSession.getTimeDataDao();
     }
 
-    TimeData getTimeData(long playerId, long gameId){
+    public TimeData getTimeData(long playerId, long gameId){
         this.queryBuilder = this.timeDataDao.queryBuilder().where(TimeDataDao.Properties.PlayerID.eq(playerId), TimeDataDao.Properties.GameID.eq(gameId));
         List<TimeData> timeDataList = this.queryBuilder.list();
         if(timeDataList.size() == 1){
             return timeDataList.get(0);
+        } else {
+            return createTimeData(playerId, gameId);
         }
-        return createTimeData(playerId, gameId);
     }
 
-    TimeData createTimeData(long playerId, long gameId){
+    public TimeData createTimeData(long playerId, long gameId){
         TimeData timeData = new TimeData();
         timeData.setPlayerID(playerId);
         timeData.setGameID(gameId);
-        timeData.setBestTimeType(new TimeType(Double.MAX_VALUE));
-        timeData.setAvgTimeType(new TimeType(1));
         timeData.setTimesPlayed(0);
-        saveTimeData(timeData);
+        timeData.setBestTimeType(new TimeType(Long.MAX_VALUE));
+        timeData.setAvgTimeType(new TimeType(1));
+        timeDataDao.save(timeData);
         return timeData;
     }
 
-
-    TimeData saveTimeData(TimeData timeData){
-        timeDataDao.save(timeData);
-        return timeData;
+    public int deleteTimeData(long playerId, long gameId) {
+        this.queryBuilder = this.timeDataDao.queryBuilder().where(TimeDataDao.Properties.PlayerID.eq(playerId), TimeDataDao.Properties.GameID.eq(gameId));
+        List<TimeData> timeDataList = this.queryBuilder.list();
+        if (timeDataList.size() == 1) {
+            timeDataDao.delete(timeDataList.get(0));
+            return 1;
+        }
+        return 0;
     }
 }
